d2356722bbd38be57a76e2a2a822c27e1f24664b
chirst <colton141@live.com>
2022-08-21 16:30:11 -0600

    format

--- lib/mars.ex
+++ lib/mars.ex
@@ -5,6 +5,14 @@ defmodule Mars do
 
   def execute_mission do
     final_rovers = for r <- Rover.Reader.file("test.txt"), do: Rover.navigate(r)
+
+
+
+
+
+
+
+
     Rover.Writer.file("out.txt", final_rovers)
   end
 end
